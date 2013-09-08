c774dd896411c5182b1ea33f770d57dd99b406ab
Sakis Bouzanis <sakisbou@yahoo.com>
2013-09-08 18:01:11 +0300

    modified:   textures.c

--- src/textures.c
+++ src/textures.c
@@ -13,10 +13,10 @@
 */
 void gl_textures_init(void)
 {
-  crate_01 = create_gl_texture("textures/crate_01.jpg");
-  crate_02 = create_gl_texture("textures/crate_02.jpg");
-  crate_03 = create_gl_texture("textures/crate_03.jpg");
-  crate_04 = create_gl_texture("textures/crate_04.jpg");
+  crate_01 = create_gl_texture("../textures/crate_01.jpg");
+  crate_02 = create_gl_texture("../textures/crate_02.jpg");
+  crate_03 = create_gl_texture("../textures/crate_03.jpg");
+  crate_04 = create_gl_texture("../textures/crate_04.jpg");
 }
 
 
