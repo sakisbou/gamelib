bf9f2bbfb08e233746155fff26b78287123b523a
sak <sak@funtop.home>
2013-10-06 15:34:30 +0300

    modified:   textures.c 	new file:   textures/earth.jpg 	new file:   textures/lander.jpg 	new file:   textures/terrain.jpg

--- src/textures.c
+++ src/textures.c
@@ -14,7 +14,7 @@
 void gl_textures_init(void)
 {
   lander = create_gl_texture("textures/lander.jpg");
-  terrain = create_gl_texture("textures/terrain-03.jpg");
+  terrain = create_gl_texture("textures/terrain.jpg");
   earth = create_gl_texture("textures/earth.jpg");
 }
 
